1afdb26eb4cb4b01f7a4a3cb7357f65a5f2d667f
arif1101 <arifurrahmanarif223@gmail.com>
2025-06-18 11:26:57 +0600

    user create

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express, { Application, Request, Response } from 'express';
 import { model, Schema } from 'mongoose';
 import { notesRoutes } from './app/controllers/notes.controller';
+import { userRoutes } from './app/controllers/user.controller';
 
 const app: Application = express();
 app.use(express.json())
 
 app.use("/notes", notesRoutes)
+app.use("/user", userRoutes)
+
+
 app.get('/', (req: Request, res: Response)=> {
     res.send("welcome to Note notesRoutes")
 })
